e23def0b5bc66317124704698b442de3949a7843
Andrew Jack <me@andrewjack.uk>
2021-04-11 15:19:11 +0100

    add gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Home = () => (
+  <div>
+    <p>Andrew Jack, Software Developer.</p>
+    <p>
+      <ul>
+        <li><a href="https://twitter.com/andr3wjack">Twitter</a></li>
+        <li><a href="https://www.linkedin.com/in/andr3wjack">LinkedIn</a></li>
+        <li><a href="https://github.com/AndrewJack">GitHub</a></li>
+        <li><a href="mailto:me@andrewjack.uk">Email</a></li>
+      </ul>
+    </p>
+  </div>
+)
+export default Home
