a82daee4182a13b6b5bf85b30dce1d6818c99461
Andrew Jack <me@andrewjack.uk>
2021-05-04 15:20:50 +0100

    Turn site into blog

---
+++ src/components/bio.js
@@ -0,0 +1,45 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          social {
+            twitter
+            linkedin
+            github
+            email
+          }
+        }
+      }
+    }
+  `)
+
+  // Set these values by editing "siteMetadata" in gatsby-config.js
+  const social = data.site.siteMetadata?.social
+
+  return (
+    <div className="bio">
+      <p>
+        {social?.twitter ? <a href={`https://twitter.com/${social?.twitter}`}>Twitter</a> : null}
+        {" • "}
+        {social?.linkedin ? <a href={social?.linkedin}>LinkedIn</a> : null}
+        {" • "}
+        {social?.github ? <a href={social?.github}>GitHub</a> : null}
+        {" • "}
+        {social?.email ? <a href={social?.email}>Email</a> : null}
+      </p>
+    </div>
+  )
+}
+
+export default Bio
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,89 @@
-import React from "react"
+import * as React from "react"
+import { Link, graphql } from "gatsby"
 
-const Home = () => (
-  <div>
-    <p>Andrew Jack, Software Developer.</p>
-    <p>
-      <ul>
-        <li><a href="https://twitter.com/andr3wjack">Twitter</a></li>
-        <li><a href="https://www.linkedin.com/in/andr3wjack">LinkedIn</a></li>
-        <li><a href="https://github.com/AndrewJack">GitHub</a></li>
-        <li><a href="mailto:me@andrewjack.uk">Email</a></li>
-      </ul>
-    </p>
-  </div>
-)
-export default Home
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <Seo title="All posts" />
+        <Bio />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="All posts" />
+      <Bio />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
